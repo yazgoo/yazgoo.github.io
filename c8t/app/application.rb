ef9d23034156d34181eb927fe37c3623d85a11ee
Olivier Abdesselam <yazgoo@gmail.com>
2014-01-22 00:37:38 +0100

    update

--- c8t/app/application.rb
+++ c8t/app/application.rb
@@ -25,6 +25,7 @@ class Runner
         @special = [' ', ',', '/', '?', '=', '\n', ':']
     end
     def encode address
+        p address
         @special.each do |s|
             address = address.gsub s, (sprintf "%%%02x", s.ord)
             p address
